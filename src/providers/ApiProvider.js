14b0e72187afbea8dfefe50e80856ba2c4450066
Michael MacLean <20232905+hanabanashiku@users.noreply.github.com>
2022-01-01 12:05:46 -0500

    Finshed auth stuff

--- src/providers/ApiProvider.js
+++ src/providers/ApiProvider.js
@@ -1,78 +1,120 @@
-import PROVIDERS from './providers';
+import PROVIDERS from "./providers";
 
 export default class ApiProvider {
-    /**
-     * @returns {Promise<number>} The selected provider.
-     * @see providers enum
-     */
-    async get selectedProvider() {
-        const result = await browser.storage.local.get({'selected_provider': PROVIDERS.UNSELECTED});
-        return result['selected_provider'];
-    };
+  /**
+   * @returns {Promise<number>} The selected provider.
+   * @see providers enum
+   */
+  async getSelectedProvider() {
+    const result = await browser.storage.local.get({
+      selected_provider: PROVIDERS.UNSELECTED,
+    });
+    return result["selected_provider"];
+  }
 
-    /**
-     * Authorize with the API.
-     * @param username {string} The username to use.
-     * @param password {string} The password to use.
-     * @returns {bool} True on success.
-     */
-    authorize() {
-        throw 'Not Implemented';
+  /**
+   * Authorize with the API.
+   * @param username {string} The username to use.
+   * @param password {string} The password to use.
+   * @returns {boolean} True on success.
+   */
+  authorize() {
+    throw "Not Implemented";
+  }
+
+  /**
+   * Refreshes the bearer token using the token saved in storage.
+   * @returns {Promise<boolean>} True on success.
+   * @throws
+   */
+  refresh() {
+    throw "Not Implemented";
+  }
+
+  /**
+   * Check if the access token is about to expire.
+   * @returns {boolean} Returns true if the token should be refreshed.
+   */
+  async shouldRefresh() {
+    const data = browser.storage.local.get({ access_token_expires_on: 0 });
+    const currentTime = Math.floor(new Date().getTime() / 1000);
+
+    return currentTime > (await data)["access_token_expires_on"] - 600;
+  }
+
+  /**
+   * Signs the user out of the api provider.
+   * @returns {Promise<void>}
+   */
+  async signOut() {
+    await browser.storage.local.remove([
+      "selected_provider",
+      "access_token",
+      "refresh_token",
+      "access_token_expires_on",
+    ]);
+  }
+
+  /**
+   * @returns {Promise<string>} The bearer token used to authenticate.
+   */
+  async getAuthToken() {
+    const result = await browser.storage.local.get({ access_token: null });
+    return result["access_token"];
+  }
+
+  /**
+   * @returns {Promise<string>} The current refresh token.
+   */
+  async getRefreshToken() {
+    const result = await browser.storage.local.get({ refresh_token: null });
+    return result["refresh_token"];
+  }
+
+  /**
+   * Sets the api provider to use.
+   * @param {number} provider The provider from the providers enum.
+   * @see providers enum.
+   */
+  async _setProvider(provider) {
+    await browser.storage.local.set("selected_provider", provider);
+  }
+
+  /**
+   * Sets the auth token data to local storage.
+   * @param {string} token The auth token to set.
+   * @param {number} expirationTick The time, in ticks, when the token will expire and need to be refreshed.
+   */
+  async _setAuthToken(token, expirationTick) {
+    await browser.storage.local.set({
+      access_token: token,
+      access_token_expires_on: expirationTick,
+    });
+  }
+
+  /**
+   * Sets the refresh token to local storage.
+   * @param {string} token The refresh token to set.
+   */
+  async _setRefreshToken(token) {
+    await browser.storage.local.set("refresh_token", token);
+  }
+
+  /**
+   *
+   * @param {import("axios").AxiosRequestConfig} config
+   * @returns {Promise<import("axios").AxiosRequestConfig>}
+   */
+  async _requestInterceptor(config) {
+    if (this.shouldRefresh()) {
+      await this.refresh();
     }
 
-    /**
-     * Refreshes the bearer token using the token saved in storage.
-     * @returns {bool} True on success.
-     * @throws
-     */
-    refresh() {
-        throw 'Not Implemented';
+    const token = await this.getAuthToken();
+    if (!token) {
+      throw Error("Call authorize before making a request.");
     }
-
-    /**
-     * @returns {Promise<string>} The bearer token used to authenticate.
-     */
-     async get authToken() {
-        const result = await browser.storage.local.get({ 'access_token': null });
-        return result['access_token'];
-    }   
-    
-
-
-    /**
-     * @returns {Promise<string>} The current refresh token.
-     */
-    async get refreshToken() {
-        const result = await browser.storage.local.get({ 'refresh_token': null });
-        return result['refresh_token'];
-    }
-
-    /**
-     * Sets the api provider to use.
-     * @param {number} provider The provider from the providers enum.
-     * @see providers enum.
-     */
-    async _setProvider(provider) {
-        await browser.storage.local.set('selected_provider', provider);
-    };
-
-    /**
-     * Sets the auth token data to local storage.
-     * @param {string} token The auth token to set.
-     * @param {number} expirationTick The time, in ticks, when the token will expire and need to be refreshed.
-     */
-    async _setAuthToken(token, expirationTick) {
-        await browser.storage.local.set({
-            'access_token': token,
-            'access_token_expires_on': expirationTick,
-        });
-    }
-        
-    /**
-     * Sets the refresh token to local storage.
-     * @param {string} token The refresh token to set.
-     */
-    async set _setRefreshToken(token) {
-        await browser.storage.local.set('refresh_token', token);
-    }
-};
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  }
+}
--- src/providers/KitsuProvider.js
+++ src/providers/KitsuProvider.js
@@ -1,57 +1,67 @@
 import axios from "axios";
 import ApiProvider from "./ApiProvider";
-import PROVIDERS from './providers';
+import PROVIDERS from "./providers";
 
 const KITSU_BASE_URL = "https://kitsu.io/api/edge";
 const KITSU_AUTH_URL = "https://kitsu.io/api/oauth";
 
 export default class KitsuProvider extends ApiProvider {
-    constructor() {
-        this.#client = axios.create({
-            baseURL: KITSU_BASE_URL,
-            headers: {
-                'Accept': 'application/vnd.api+json',
-                'Content-Type': 'application/vnd.api+json'
-            }
-        });
-        this._setProvider(PROVIDERS.KITSU);
-    }
+  /**
+   * @type {import("axios").AxiosInstance}
+   */
+  #client = null;
 
-    async authorize(username, password) {
-        const params = new URLSearchParams();
-        params.append('grant_type', 'password');
-        params.append('username', username);
-        params.append('password', password);
-        params.append('scope', '');
-        
-        try {
-            const response = await axios.post(`${KITSU_AUTH_URL}/token`, params);
-            await this.#setTokenResponse(response);
-            return true;
-        }
-        catch(e) {
-            throw "Unable to authenticate. Check username/password";
-        }
-    }
+  constructor() {
+    super();
+    this.#client = axios.create({
+      baseURL: KITSU_BASE_URL,
+      headers: {
+        Accept: "application/vnd.api+json",
+        "Content-Type": "application/vnd.api+json",
+      },
+    });
+    this.#client.interceptors.request.use(async (config) => {
+      return this._requestInterceptor(config);
+    });
+    this._setProvider(PROVIDERS.KITSU);
+  }
 
-    async refresh() {
-        const token = this.refreshToken;
-        const params = new URLSearchParams();
-        params.append('grant_type', 'refresh_token');
-        params.append('refresh_token', await token);
+  async authorize(username, password) {
+    const params = new URLSearchParams();
+    params.append("grant_type", "password");
+    params.append("username", username);
+    params.append("password", password);
+    params.append("scope", "");
 
-        try {
-            const response = await axios.post(`${KITSU_AUTH_URL}/token`, params);
-            await this.#setTokenResponse(response);
-            return true;
-        }
-        catch {
-            throw "Unable to authenticate. Check refresh token.";
-        }
+    try {
+      const response = await axios.post(`${KITSU_AUTH_URL}/token`, params);
+      await this.#setTokenResponse(response);
+      return true;
+    } catch (e) {
+      throw "Unable to authenticate. Check username/password";
     }
+  }
 
-    async #setTokenResponse(response) {
-        this._setAuthToken(response.data["access_token"], response.data["created_at"] + response.data["expires_in"]);
-        this._setRefreshToken(response.data["refresh_token"]);
+  async refresh() {
+    const token = this.refreshToken;
+    const params = new URLSearchParams();
+    params.append("grant_type", "refresh_token");
+    params.append("refresh_token", await token);
+
+    try {
+      const response = await axios.post(`${KITSU_AUTH_URL}/token`, params);
+      await this.#setTokenResponse(response);
+      return true;
+    } catch {
+      throw "Unable to authenticate. Check refresh token.";
     }
-};
+  }
+
+  async #setTokenResponse(response) {
+    this._setAuthToken(
+      response.data["access_token"],
+      response.data["created_at"] + response.data["expires_in"]
+    );
+    this._setRefreshToken(response.data["refresh_token"]);
+  }
+}
